9d9900f06a5ead44daf3dfcbdae982a4645b21ca
Ivan <Bojenka229@gmail.com>
2021-04-28 23:26:44 +0300

    + add button

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import button from "./components/button";
 
 Vue.config.productionTip = false;
+
+export default button;
 
 new Vue({
   render: (h) => h(App),
